05c86fdb2b6bd8aacacdd74717a6916722c11833
Jan Bock <j.g.bock@outlook.com>
2022-09-27 20:27:37 +0100

    new page styling, setup for primary logic

--- crud-notes-app/crud-notes-react/src/Screens/NotesHomepageScreen.js
+++ crud-notes-app/crud-notes-react/src/Screens/NotesHomepageScreen.js
@@ -1,20 +1,21 @@
 import "../App.css";
+import AllNotesImg from "../images/all-notes-page.png";
 
 import React from "react";
 
-function NotesHomepageScreen() {
+const NotesHomepageScreen = () => {
   return (
     <>
-        <p>All Notes</p>
-    <div>
-        <button>sort</button>
-        <button>My Profile</button>
-        <button>search</button>
+        <p className="custom-font all-notes-header" id="profile-icon">profile</p>
+    <div className="all-notes-top-btns">
+        <button className="all-notes-btn">sort</button>
+        <button className="all-notes-btn custom-font all-notes-header">All Notes</button>
+        <button className="all-notes-btn">sear</button>
     </div>
-      <img placeholder="hello" alt="alt text"/>
-      <h2>Create your first note!</h2>
-      <p>Add a note about anything (your thoughts on climate change, or your history essay) and share it witht the world.</p>
-      <button>Create A Note</button>
+      <img className="premium-img all-notes-img" placeholder="shelf with random items" alt="all notes page" src={AllNotesImg}/>
+      <h2 className="screen-title">Create your first note!</h2>
+      <p className="app-default-font all-notes-desc">Add a note about anything (your thoughts on climate change, or your history essay) and share it witht the world.</p>
+      <button className="introduction-button-group create-note-btn">CREATE A NOTE</button>
     </>
   );
 }
